2fbc8979fe7e335f0781bd51c848c60b1cc2172c
okurut george <okurut17@gmail.com>
2023-07-31 07:54:54 +0400

    first

--- routes/auth.js
+++ routes/auth.js
@@ -44,6 +44,9 @@ router.post("/register", async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ error: "Email already registered" });
     }
+
+    const saltRounds = 10;
+
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     // Create a new user
